52fa82a5cbd489d4258e1f259dac9edb80681d74
Felicia Fredlund <felicia.fredlund@gmail.com>
2025-06-20 18:43:54 +0200

    add get_files_info.py and tests for it in tests.py

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,19 +1,30 @@
-def get_files_info(working_directory, directory = None):
-    files_info = ""
-    # if directory is outside working_directory.
-    # return this string
-    # f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+import os
+
+def get_files_info(working_directory: str, directory: str = None) -> str:
+    if working_directory == None:
+        return "Error: No working directory specified."
     
-    # if directory is not a directory
-    # return this string
-    # f'Error: "{directory}" is not a directory'
+    if directory.startswith("/") or directory.startswith(".."):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    # If any errors are raised by the standard library functions,
-    # catch them and instead return a string describing the error.
-    # Always prefix error strings with "Error:".
+    if directory == None:
+        directory = "."
 
-    # Format for the string if no errors occured:
-    # - README.md: file_size=1032 bytes, is_dir=False
-    # - src: file_size=128 bytes, is_dir=True
-    # - package.json: file_size=1234 bytes, is_dir=False
-    return files_info
+    relative_path_to_directory = os.path.join(working_directory, directory)
+    abs_path_to_directory = os.path.abspath(relative_path_to_directory)
+
+    if not os.path.isdir(abs_path_to_directory):
+        return f'Error: "{directory}" is not a directory'
+
+
+    files_info = []
+    for filename in os.listdir(abs_path_to_directory):
+        try:
+            path = os.path.join(abs_path_to_directory, filename)
+            file_size = os.path.getsize(path)
+            is_dir = os.path.isdir(path)
+            files_info.append(f"- {filename}: {file_size=} bytes, {is_dir=}")
+        except Exception as e:
+            return "Error: there was an error while listing files. See more info: {e}"
+
+    return "\n".join(files_info)
