792db8ff1297b10b7e7155374f8a5b0e4136ebc7
Felicia Fredlund <felicia.fredlund@gmail.com>
2025-07-01 19:07:01 +0200

    move generate_content to llm_instructions.py

--- llm_instructions.py
+++ llm_instructions.py
@@ -4,15 +4,6 @@ from functions.get_file_content import schema_get_file_content
 from functions.run_python_file import schema_run_python_files
 from functions.write_file_content import schema_write_file_content
 
-available_functions = types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_files,
-        schema_write_file_content
-    ]
-)
-
 system_prompt = """
 You are a helpful AI coding agent.
 
@@ -25,3 +16,32 @@ When a user asks a question or makes a request, make a function call plan. You c
 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
+
+available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+        schema_get_file_content,
+        schema_run_python_files,
+        schema_write_file_content
+    ]
+)
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+        config=types.GenerateContentConfig(
+            tools=[available_functions],
+            system_instruction=system_prompt
+        ),
+    )
+
+    if verbose:
+        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print("Response tokens:", response.usage_metadata.candidates_token_count)
+    
+    if not response.function_calls:
+        return response.text
+    else:
+        for function_call_part in response.function_calls:
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
-from llm_instructions import system_prompt, available_functions
+from llm_instructions import generate_content
 
 def main():
     load_dotenv()
@@ -34,27 +34,6 @@ def main():
     ]
 
     generate_content(client, messages, verbose)
-
-
-def generate_content(client, messages, verbose):
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            tools=[available_functions],
-            system_instruction=system_prompt
-        ),
-    )
-
-    if verbose:
-        print("Prompt tokens:", response.usage_metadata.prompt_token_count)
-        print("Response tokens:", response.usage_metadata.candidates_token_count)
-    
-    if not response.function_calls:
-        return response.text
-    else:
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
 
 
 if __name__ == "__main__":
