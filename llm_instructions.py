467ccf4032876b9bde321dadfe3d43740fff4a58
Felicia Fredlund <felicia.fredlund@gmail.com>
2025-07-01 21:24:53 +0200

    add call_function to llm_instructions

--- llm_instructions.py
+++ llm_instructions.py
@@ -1,8 +1,8 @@
 from google.genai import types
-from functions.get_files_info import schema_get_files_info
-from functions.get_file_content import schema_get_file_content
-from functions.run_python_file import schema_run_python_files
-from functions.write_file_content import schema_write_file_content
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.run_python_file import schema_run_python_files, run_python_file
+from functions.write_file_content import schema_write_file_content, write_file_content
 
 system_prompt = """
 You are a helpful AI coding agent.
@@ -26,6 +26,47 @@ available_functions = types.Tool(
     ]
 )
 
+def call_function(function_call_part, verbose=False):
+    function_name = function_call_part.name
+    function_args = function_call_part.args
+    
+    if verbose:
+        print(f"Calling function: {function_name}({function_args})")
+    else:
+        print(f" - Calling function: {function_name}")
+
+    functions = {
+        "get_files_info": get_files_info,
+        "get_file_content": get_file_content,
+        "run_python_file": run_python_file,
+        "write_file_content": write_file_content
+    }
+
+    if function_name not in functions:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"error": f"Unknown function: {function_name}"},
+                )
+            ],
+        )
+    
+    function_args["working_directory"] = "./calculator"
+    function_result = functions[function_name](**function_args)
+
+    return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=function_name,
+                    response={"result": function_result},
+                )
+            ],
+        )
+
+
 def generate_content(client, messages, verbose):
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
@@ -42,6 +83,14 @@ def generate_content(client, messages, verbose):
     
     if not response.function_calls:
         return response.text
-    else:
-        for function_call_part in response.function_calls:
-            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
+    for function_call_part in response.function_calls:
+        function_call_result = call_function(function_call_part, verbose)
+
+        try:
+            response = function_call_result.parts[0].function_response.response
+
+            if verbose:
+                print(f"-> {response}")
+        except:
+            raise Exception(f"Error: llm_instructions.call_function returned incorrectly.")
